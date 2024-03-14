7ba4d0fbed246375d8beff3aec05005725a76787
Abodi Massarwa <abodi.msarwi@gmail.com>
2024-03-14 16:38:44 +0200

    first commit with template of a good docstring

---
+++ DemoDocTest/main.py
@@ -0,0 +1,23 @@
+def your_function_name(parameters):
+    """
+    Description of what your function does.
+
+    Parameters:
+    ----------
+    parameter1 : type
+        Description of parameter1.
+    parameter2 : type
+        Description of parameter2.
+    ...
+
+    Returns:
+    -------
+    return_type
+        Description of what the function returns.
+
+    Examples:
+    --------
+    >>> your_function_name(example_parameter1, example_parameter2)
+    Expected_output
+    """
+    # Your function code goes here
